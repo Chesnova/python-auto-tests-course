86853b9bd37216eb268276bcc8348a9a16cd8c71
Chesnova <myjobistester@gmail.com>
2022-03-22 22:06:45 +0300

    add new file

--- lesson16_step5.py
+++ lesson16_step5.py
@@ -1,12 +1,17 @@
+import math
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
 browser = webdriver.Chrome()
-try:
-    browser.get(link)
+browser.get("http://suninjuly.github.io/find_link_text")
 
+text = str(math.ceil(math.pow(math.pi, math.e)*10000))
+print(text)
+link = browser.find_element(By.LINK_TEXT, f"{text}")
+
+try:
+    link.click()
     input1 = browser.find_element(By.NAME, "first_name")
     input1.send_keys("Ivan")
     input2 = browser.find_element(By.NAME, "last_name")
@@ -21,5 +26,3 @@ try:
 finally:
     time.sleep(5)
     browser.quit()
-
-# python lesson16_step4.py
